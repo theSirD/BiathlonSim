977d91314135975fe6f20cac4229f44824a146cc
theSirD <isaev-daniel17@yandex.ru>
2025-05-06 22:13:55 +0300

    feat: adds main.go

--- main.go
+++ main.go
@@ -1,21 +1,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	configFile := flag.String("config", "config.json", "Path to the configuration file")
+	eventsFile := flag.String("events", "events", "Path to the events file")
+	flag.Parse()
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	baseDir := "."
+	exePath, err := os.Executable()
+	if err == nil {
+		baseDir = filepath.Dir(exePath)
 	}
+
+	absConfigFile := *configFile
+	if !filepath.IsAbs(absConfigFile) {
+		absConfigFile = filepath.Join(baseDir, *configFile)
+	}
+
+	absEventsFile := *eventsFile
+	if !filepath.IsAbs(absEventsFile) {
+		absEventsFile = filepath.Join(baseDir, *eventsFile)
+	}
+
+	cfg, err := LoadConfig(absConfigFile)
+	if err != nil {
+		log.Fatalf("Error loading configuration: %v", err)
+	}
+	fmt.Printf("Configuration loaded from %s: %+v\n\n", absConfigFile, cfg)
+
+	incomingEvents, err := LoadEvents(absEventsFile)
+	if err != nil {
+		log.Fatalf("Error loading events: %v", err)
+	}
+	fmt.Printf("Loaded %d events from %s.\n\n", len(incomingEvents), absEventsFile)
+
+	simulation := NewSimulation(cfg)
+
+	simulation.Run(incomingEvents)
+	simulation.FinalizeResults()
+
+	GenerateOutputLog(simulation.OutputLog)
+	GenerateFinalReport(simulation.Competitors, cfg)
+
+	fmt.Println("\nBiathlonSim finished.")
 }
